1cf9dd03712444935652ff50927d979974fad63c
pgri999 <grizard.pierre@gmail.com>
2026-01-09 07:50:49 -0500

    Commit2

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 #function to run emotion detection
 def emotion_detector(text_to_analyze):
@@ -15,5 +16,17 @@ def emotion_detector(text_to_analyze):
     #sending the request
     response = requests.post(url, json = myobj, headers=headers)
 
-    
-    return response.text
+    #converting the request in json
+    formatted_response=json.loads(response.text)
+
+    #extracting only the emotions from the response
+    dico=formatted_response["emotionPredictions"][0]
+    emotions = dico['emotion']
+
+    # Find the key with the highest value
+    dominant_key = max(emotions, key=emotions.get)
+
+    # Add a new key 'dominant_emotion' to the dictionary
+    emotions['dominant_emotion'] = dominant_key
+
+    return emotions
